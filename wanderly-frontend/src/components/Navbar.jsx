37db660a6c98bd33a81c17eb9d679ffeffb495e4
zishanx <zishanalikhan53@gmail.com>
2026-06-10 09:41:06 +0530

    Auth Rename

--- wanderly-frontend/src/components/Navbar.jsx
+++ wanderly-frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/Authcontext";
 import { useState } from "react";
 
 export default function Navbar() {
