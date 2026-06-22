0e51c78b8c26bf33bd646a5c7be4a00d246564dc
zishanx <zishanalikhan53@gmail.com>
2026-06-22 21:58:51 +0530

    Trying to fix

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -5,9 +5,9 @@ import { getMyBooking } from "../controllers/gmbcontroller.js";
 
 const router = express.Router()
 
-router.post('/booking/create', protect, createBooking)
+router.post('/create', protect, createBooking)
 
-router.post('/booking/verify', protect, verifyPayment)
+router.post('/verify', protect, verifyPayment)
 
 router.get('/all', protect, getMyBooking)
 
